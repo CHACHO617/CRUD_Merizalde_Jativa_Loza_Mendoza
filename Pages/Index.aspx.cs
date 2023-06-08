215be1cbaca2bcf7c4212c2c5b5f52951719b5d7
EnriqueMerizalde <emerizalde61702@gmail.com>
2023-06-08 11:03:43 -0500

    Add project files.

---
+++ Pages/Index.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using CRUD.Singleton;
+
+namespace CRUD.Pages
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        readonly SqlConnection con = Singleton.Singleton.Instance;
+        
+        readonly Log log1 = Log.Instance;
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CargarTabla();
+        }
+
+        void CargarTabla()
+        {
+            SqlCommand cmd = new SqlCommand("sp_load", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            SqlDataAdapter da= new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            gvusuarios.DataSource = dt;
+            gvusuarios.DataBind();
+            con.Close();
+        }
+
+        protected void BtnCreate_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Pages/CRUD.aspx?op=C");
+            log1.guardarlog(Title);
+        }
+
+        protected void BtnRead_Click(object sender, EventArgs e)
+        {
+            string id;
+            Button BtnConsultar = (Button)sender;
+            GridViewRow selectedrow = (GridViewRow)BtnConsultar.NamingContainer;
+            id = selectedrow.Cells[1].Text;
+            Response.Redirect("~/Pages/CRUD.aspx?id="+id+"&op=R");
+        }
+
+        protected void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            string id;
+            Button BtnConsultar = (Button)sender;
+            GridViewRow selectedrow = (GridViewRow)BtnConsultar.NamingContainer;
+            id = selectedrow.Cells[1].Text;
+            Response.Redirect("~/Pages/CRUD.aspx?id=" + id + "&op=U");
+        }
+
+        protected void BtnDelete_Click(object sender, EventArgs e)
+        {
+            string id;
+            Button BtnConsultar = (Button)sender;
+            GridViewRow selectedrow = (GridViewRow)BtnConsultar.NamingContainer;
+            id = selectedrow.Cells[1].Text;
+            Response.Redirect("~/Pages/CRUD.aspx?id=" + id + "&op=D");
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.Singleton
+{
+    public class Singleton
+    {
+        private readonly static SqlConnection _instance = new SqlConnection(ConfigurationManager.ConnectionStrings["conexion"].ConnectionString);
+
+        public static SqlConnection Instance //Creamos el acceso al atributo _instance
+        {
+            get { return _instance; }
+        }
+        private Singleton()  //Constructyor private para no se puedan crear objetos de esta clase
+        {
+
+        }
+    }
+}
